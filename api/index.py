76065f0d1e98ab64e5047477bd5b3848bce5a7d8
JinhuaSu <55989557+JinhuaSu@users.noreply.github.com>
2024-03-29 10:26:17 +0000

    fastapi

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/func/')
-def home():
-    return 'Hello, World!'
+# @app.route('/func/')
+# def home():
+#     return 'Hello, World!'
 
-@app.route('/func/about')
-def about():
-    return 'About'
+# @app.route('/func/about')
+# def about():
+#     return 'About'
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/api/python")
+def hello_world():
+    return {"message": "Hello World"}
